4ebc705b15b7eaa5ac6be11eb76af2a1ec2076d3
lgentaz <lucile.gentaz@gmail.com>
2020-04-18 11:32:01 +0200

    samedi

--- google_searcher.rb
+++ google_searcher.rb
@@ -12,12 +12,25 @@ search_bar = browser.text_field(class: "gsfi")
 search_bar.set(search_what)
 
 #appuie sur entrer
-search_bar.send_keys(:enter)
+
+(search_bar.send_keys(:enter))
+
+browser.driver.manage.timeouts.implicit_wait = 3
+#!#browser.window(title: "Foo")wait_while(&:exists?)
+
+search_result_h3s = browser.h3s(class:"rc")
+search_result_h3s.each { |h3| puts h3.text }
+
+p "Méfait accompli, fermeture du browser"
+
+browser.close
+
+#pour empecher la fermeture de la fenetre
+#!#while true
+#!#end
 
 #méthode du clic
-##submit_button = browser.button(type:"submit")
-##submit_button.click
-
-sleep (30)
+#!#submit_button = browser.button(type:"submit")
+#!#submit_button.click
 
 Launchy.open("http://stackoverflow.com")
